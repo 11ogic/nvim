9a4bfd838e73a2f1b22fe318b703e138f58e721b
haven <haven@hellotalk.cn>
2024-07-14 20:13:21 +0800

    init

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
-require("core.plugins")
-require("core.options")
-require("core.keymaps")
-
--- Load machine specific configuration
-local config_path = vim.fn.stdpath("config")
-local current_config_path = config_path .. "/lua/config/machine_specific.lua"
-if not vim.loop.fs_stat(current_config_path) then
-  local current_config_file = io.open(current_config_path, "wb")
-  local default_config_path = config_path .. "/default_config/_machine_specific_default.lua"
-  local default_config_file = io.open(default_config_path, "rb")
-  if default_config_file and current_config_file then
-    local content = default_config_file:read("*all")
-    current_config_file:write(content)
-    io.close(default_config_file)
-    io.close(current_config_file)
-  end
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-require("config.machine_specific")
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,79 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-  "nvim-treesitter/playground",
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    priority = 1000,
-    build = ":TSUpdate",
-    config = function()
-      vim.opt.smartindent = false
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        sync_install = false,
-        ensure_installed = {
-          "markdown",
-          "html",
-          "javascript",
-          "typescript",
-          "vue",
-          "tsx",
-          "query",
-          "dart",
-          "java",
-          "c",
-          "prisma",
-          "bash",
-          "go",
-          "lua",
-          "kdl",
-          "vim",
-          "terraform",
-          "dockerfile",
-          "yaml",
-          "python",
-        },
-        highlight = {
-          enable = true,
-          disable = {}, -- list of language that will be disabled
-        },
-        indent = {
-          enable = true
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection    = "<leader>n",
-            node_incremental  = "<leader>k",
-            node_decremental  = "<leader>j",
-            scope_incremental = "<leader>l",
-          },
-        },
-        ignore_install = {},
-        modules = {}
-      })
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-      local tscontext = require('treesitter-context')
-      tscontext.setup {
-        enable = true,
-        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit
-        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20,     -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      }
-      vim.keymap.set("n", "<c-j>", function()
-        tscontext.go_to_context()
-      end, { silent = true })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
   },
 }
