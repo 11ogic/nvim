7234eed7c23e794855bc940444df8ab1eed0dc18
zerotwo <1030516167@qq.com>
2025-05-10 18:42:52 +0800

    fix the loading time of LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   -- LSP配置
   {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
