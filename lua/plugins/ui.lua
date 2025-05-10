1a1a088f5a90d327a49e6ae8a7dfea47495e092a
zerotwo <1030516167@qq.com>
2025-05-10 20:28:22 +0800

    configure the nvim-tree plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -55,6 +55,43 @@ return {
         filters = {
           dotfiles = false,
         },
+        -- file actions
+        actions = {
+          open_file = {
+            window_picker = {
+              enable = true,
+            },
+          },
+        },
+        -- custom key mappings
+        on_attach = function(bufnr)
+          local api = require('nvim-tree.api')
+          
+          local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+          vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+          vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+          vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+          vim.keymap.set('n', 't', api.node.open.tab, opts('Open: New Tab'))
+          vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+          vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+          vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+          vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+          vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+          vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+          vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+          vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+          vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+          vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+          vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+          vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+          vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+          vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+        end,
       })
 
       -- 设置按键映射
